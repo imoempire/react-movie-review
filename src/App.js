8c6428c6c5d50218a4d948b0edce33ee9540c6d2
imoempire <mrimoempire@gmail.com>
2021-08-05 12:13:10 +0000

    import components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import './Components/App.css';
+import Class from './Components/Class';
+import Top from './Components/Top';
 
 function App() {
   return (
-    <div className="App">
-      
+    <>
+     <Top/> 
+    <div className="container-fluid">
+      <div className="row">
+      <div className="col">
+        <Class/>
+        </div>
+      </div>
     </div>
+    </>
   );
 }
 
