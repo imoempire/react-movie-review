d99d9a4c1576b0d78681f4b3f468d346f493411d
imoempire <mrimoempire@gmail.com>
2021-08-05 13:15:17 +0000

    silder label

--- src/Components/Top.jsx
+++ src/Components/Top.jsx
@@ -27,8 +27,8 @@ export default function Top() {
             alt="First slide"
           />
           <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>Get Your Movie reviews Here</h3>
+            <p>More</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item className="Item">
@@ -39,8 +39,8 @@ export default function Top() {
           />
 
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3>SGet Your Movie reviews Here</h3>
+            <p>More</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item className="Item">
@@ -51,10 +51,8 @@ export default function Top() {
           />
 
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
+            <h3>Get Your Movie reviews Here</h3>
+            <p>More</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
